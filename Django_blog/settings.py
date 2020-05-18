f6dc6436fd8dfe881b5f13d416f920ad8af690e7
Deepthi <saideepthi2001.com>
2020-05-18 17:02:59 +0530

    initial commit

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0xn$b!t+^50pd5p@j()npcz#!gk#%lx#zzq*tkh^f(x1&&d_61'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [' posturarticle.herokuapp.com']
 
 
 # Application definition
@@ -119,9 +119,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+import dj_database_url
+        db_from_env = dj_database_url.config(conn_max_age=500)
+		DATABASES['default'].update(db_from_env)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    			os.path.join(BASE_DIR, "static"),
+			]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 

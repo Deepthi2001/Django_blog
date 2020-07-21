b8b88fb72e481263b8f47bf5c0b3b43b167186f8
Saideepthi <49442816+Deepthi2001@users.noreply.github.com>
2020-07-21 17:06:04 +0530

    Update settings.py

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -136,6 +136,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'saideepthi2001@gmail.com'
-EMAIL_HOST_PASSWORD = 'deepthisai@1'
+EMAIL_HOST_PASSWORD = '*************'
 
 django_heroku.settings(locals())

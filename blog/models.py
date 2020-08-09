62212b54188fd024a6189fa9c2e06d8284e624f1
Deepthi <saideepthi2001.com>
2020-08-09 23:47:41 +0530

    s

--- blog/models.py
+++ blog/models.py
@@ -9,7 +9,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=datetime.datetime.now)
-    updated_on = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:

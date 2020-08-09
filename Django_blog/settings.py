9f0c1f1d3c913d008c5ef09dc7d158269bfa4533
Deepthi <saideepthi2001.com>
2020-08-09 23:36:00 +0530

    s

--- Django_blog/settings.py
+++ Django_blog/settings.py
@@ -146,9 +146,7 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 # Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
